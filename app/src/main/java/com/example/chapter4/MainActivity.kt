861ca4b9b153f6979328d20f4c23c568e302ee81
deny <ndennyh@gmail.com>
2022-03-24 22:09:40 +0700

    First Commit

---
+++ app/src/main/java/com/example/chapter4/MainActivity.kt
@@ -0,0 +1,142 @@
+package com.example.chapter4
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast.*
+import androidx.cardview.widget.CardView
+
+class MainActivity : AppCompatActivity() {
+    private var myChoice: String = ""
+    private var comChoice: String = ""
+
+    var cardview4: CardView? = null
+    var cardview5: CardView? = null
+    var cardview6: CardView? = null
+    var tvTengah: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val buttonRetry = findViewById<Button>(R.id.button1)
+
+        val cardview1 = findViewById<CardView>(R.id.cardimage1)
+        val cardview2 = findViewById<CardView>(R.id.cardimage2)
+        val cardview3 = findViewById<CardView>(R.id.cardimage3)
+
+        cardview4 = findViewById(R.id.cardimage4)
+        cardview5 = findViewById(R.id.cardimage5)
+        cardview6 = findViewById(R.id.cardimage6)
+        tvTengah = findViewById(R.id.tvTengah)
+
+        fun backgroundT() {
+            cardview1.setCardBackgroundColor(Color.TRANSPARENT)
+            cardview2.setCardBackgroundColor(Color.TRANSPARENT)
+            cardview3.setCardBackgroundColor(Color.TRANSPARENT)
+            cardview4?.setCardBackgroundColor(Color.TRANSPARENT)
+            cardview5?.setCardBackgroundColor(Color.TRANSPARENT)
+            cardview6?.setCardBackgroundColor(Color.TRANSPARENT)
+            tvTengah?.text = getString(R.string.titlevs)
+            tvTengah?.setBackgroundColor(Color.TRANSPARENT);
+        }
+
+        fun retryButton(): Boolean {
+            var flag = true
+            if (cardview1.cardBackgroundColor.isOpaque || cardview2.cardBackgroundColor.isOpaque || cardview3.cardBackgroundColor.isOpaque) flag =
+                false
+            return flag
+        }
+
+        backgroundT()
+
+        cardview1.setOnClickListener {
+            if (retryButton()) {
+                backgroundT();cardview1.setCardBackgroundColor(Color.parseColor("#c3dae9"));myChoice =
+                    "gunting";Log.d("Player Memilih:", "Gunting");hasil()
+            }
+        }
+
+        cardview2.setOnClickListener {
+            if (retryButton()) {
+                backgroundT();cardview2.setCardBackgroundColor(Color.parseColor("#c3dae9"));myChoice =
+                    "kertas";Log.d("Player Memilih:", "Kertas");hasil()
+            }
+        }
+
+        cardview3.setOnClickListener {
+            if (retryButton()) {
+                backgroundT(); cardview3.setCardBackgroundColor(Color.parseColor("#c3dae9"));myChoice =
+                    "batu";Log.d("Player Memilih:", "Batu");hasil()
+            }
+        }
+
+        buttonRetry.setOnClickListener { backgroundT() }
+
+    }
+
+    fun hasil() {
+        val cpu = (1..3).random()
+        if (cpu == 1) {
+            comChoice = "gunting";cardview4?.setCardBackgroundColor(Color.parseColor("#c3dae9"))
+        }
+        if (cpu == 2) {
+            comChoice = "kertas";cardview5?.setCardBackgroundColor(Color.parseColor("#c3dae9"))
+        }
+        if (cpu == 3) {
+            comChoice = "batu";cardview6?.setCardBackgroundColor(Color.parseColor("#c3dae9"))
+        }
+        if (myChoice == comChoice) {
+            tvTengah?.text = getString(R.string.hasilDraw);Log.d("Hasil Permainan:", "Draw");
+        } else if (myChoice == "gunting") {
+            when (comChoice) {
+                "kertas" -> {
+                    tvTengah?.text = getString(R.string.player1Menang);Log.d(
+                        "Hasil Permainan:",
+                        "Pemain 1 Menang"
+                    );tvTengah?.setBackgroundColor(Color.GREEN);
+                }
+                "batu" -> {
+                    tvTengah?.text = getString(R.string.player2Menang);Log.d(
+                        "Hasil Permainan:",
+                        "COM Menang"
+                    );tvTengah?.setBackgroundColor(Color.BLUE);
+                }
+            }
+        } else if (myChoice == "kertas") {
+            when (comChoice) {
+                "batu" -> {
+                    tvTengah?.text = getString(R.string.player1Menang);Log.d(
+                        "Hasil Permainan:",
+                        "Pemain 1 Menang"
+                    );tvTengah?.setBackgroundColor(Color.GREEN);
+                }
+                "gunting" -> {
+                    tvTengah?.text = getString(R.string.player2Menang);Log.d(
+                        "Hasil Permainan:",
+                        "COM Menang"
+                    );tvTengah?.setBackgroundColor(Color.BLUE);
+                }
+            }
+        } else if (myChoice == "batu") {
+            when (comChoice) {
+                "gunting" -> {
+                    tvTengah?.text = getString(R.string.player1Menang);Log.d(
+                        "Hasil Permainan:",
+                        "Pemain 1 Menang"
+                    );tvTengah?.setBackgroundColor(Color.GREEN);
+                }
+                "kertas" -> {
+                    tvTengah?.text = getString(R.string.player2Menang);Log.d(
+                        "Hasil Permainan:",
+                        "COM Menang"
+                    );tvTengah?.setBackgroundColor(Color.BLUE);
+                }
+            }
+        }
+    }
+
+
+}
